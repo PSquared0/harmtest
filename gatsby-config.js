3ba0e2354a57ba6f672fbd64253b6c9c6359fd98
Pascale L Pierre <Psquared@MacBook-Pro-2.local>
2024-05-28 21:03:05 -0500

    updated json

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-netlify",
     "gatsby-plugin-vanilla-extract",
     {
       resolve: "gatsby-plugin-manifest",
